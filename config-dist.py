27ff459624284217fadd7caae24e8c4295319862
sf <530170234@qq.com>
2018-03-07 22:50:16 +0800

    test at auto reply

---
+++ config-dist.py
@@ -0,0 +1,3 @@
+CONFIG = {
+    "tuling_api_key": 'xxx'
+};
---
+++ index.py
@@ -0,0 +1,19 @@
+# _*_ coding:utf-8 _*_
+
+
+from wxpy import *
+import config
+bot = Bot()
+tuling = Tuling(api_key= config.CONFIG['tuling_api_key'])
+
+# my_friend = ensure_one(bot.friends().search('大华'))
+
+@bot.register(Group, TEXT)
+def print_group_msg(msg):
+    if msg.is_at == True:
+        tuling.do_reply(msg, True)
+
+@bot.register(bot.friends())
+def reply_my_friend(msg):
+    tuling.do_reply(msg)
+embed()
